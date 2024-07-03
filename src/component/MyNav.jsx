f97ef257dfbb98c39809d5be4622df45c36b75c3
ageria20 <ageria20@gmail.com>
2024-07-03 19:54:55 +0200

    update Navbar con categories

--- src/component/MyNav.jsx
+++ src/component/MyNav.jsx
@@ -1,12 +1,21 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import fantasy from '../data/fantasy.json'
+import {useState, useEffect} from 'react'
 
-const MyNav = () => (
+
+const MyNav = props => {
+
+  const [libraries, setLibraries] = useState([]);
+
+  useEffect(() => {
+    setLibraries(fantasy)
+  }, [])
+return (
   <Navbar
     expand="lg"
     className="bg-body-tertiary mb-3"
     bg="dark"
-    data-bs-theme="dark"
-  >
+    data-bs-theme="dark">
     <Container fluid>
       <Navbar.Brand href="#">EpiBooks</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -16,9 +25,15 @@ const MyNav = () => (
           <Nav.Link href="#">About</Nav.Link>
           <Nav.Link href="#">Browse</Nav.Link>
         </Nav>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          {libraries.map(category => {
+          <NavDropdown.Item href="#">{category}</NavDropdown.Item>
+        })}
+        </NavDropdown>
       </Navbar.Collapse>
     </Container>
   </Navbar>
-)
+  )
+}
 
 export default MyNav
